8c974a2b4f34a04638ea45eff2403f8b9295bd30
Tony <tonysoto915@gmail.com>
2025-03-26 13:18:24 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary routing components
+import Stockwidget from './components/widget/stockwidget';
+import LandingPage from './pages/landing_page';
+import NavBar from './components/common/navigation/NavBar';
+
+
+import Home from './pages/home';
+import News from './pages/news';
+import Login from './components/common/auth/login';
+import Stocks from './pages/stocks';
+import Crypto from './pages/crypto';
+
+import { AlpacaProvider } from './providers/AlpachaProvider';
+import './styles/styles.css';
+import './styles/widget.css';
+import Widget from './components/widget/widget';
+
+const App: React.FC = () => {
+ 
+  return (
+    <AlpacaProvider>
+
+    <Router>
+      <NavBar /> {/* This will render the navigation bar across all pages */}
+    
+    <div >
+<Widget/>
+  </div>
+      <Routes> 
+      
+      <Route path="/" element={<LandingPage/>} /> 
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/news" element={<News />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/crypto" element={<Crypto />} />
+        <Route path="/stocks" element={<Stocks />} />  
+      </Routes>
+    </Router>
+</AlpacaProvider>
+
+  );
+};
+
+export default App;
+
---
+++ src/pages/landing_page.tsx
@@ -0,0 +1,59 @@
+
+import React from 'react';
+import '../styles/landing_page.css';
+import '../styles/styles.css';
+ 
+
+import Footer from '../components/footer';
+import CryptoCard from '../components/crypto_card';
+
+const LandingPage: React.FC = () => {
+
+  return (
+
+    <div className="landing-page">
+
+      <header className="hero-section">
+        <div className="hero-content">
+          <h1>Your Ultimate Stock Information Hub</h1>
+          <p>Stay ahead with real-time stock news, insights, and community discussions.</p>
+          <a href="/login" className="cta-btn">Get Started</a>
+        </div>
+        <div className="hero-image">
+        </div>
+      </header>
+
+      <section id="features" className="features-section">
+        <h2>Features</h2>
+        <div className="feature-cards">
+
+          <CryptoCard/>
+          <div className="feature-card">
+            <h3>Company News</h3>
+            <p>Latest news and updates from top companies.</p>
+          </div>
+          <div className="feature-card">
+            <h3>Historical Data</h3>
+            <p>Access to historical stock and crypto data.</p>
+          </div>
+          <div className="feature-card">
+            <h3>Congress Tracker</h3>
+            <p>Track congressional activities impacting the market.</p>
+          </div>
+          <div className="feature-card">
+            <h3>Buy/Sell/Hold Meter</h3>
+            <p>Insights on when to buy, sell, or hold your investments.</p>
+          </div>
+          <div className="feature-card">
+            <h3>Community Forum</h3>
+            <p>Engage with a community of investors and traders.</p>
+          </div>
+        </div>
+      </section>
+      <Footer/>
+
+    </div>
+  );
+};
+
+export default LandingPage;
